4f1badd6bd78b13f4b75aea076782d49b4092cb0
Henry Soares <Henrysoares0723@gmail.com>
2026-09-24 21:22:56 -0300

    Tratando base de dados e removendo valores vazios

--- Codigo.py
+++ Codigo.py
@@ -2,4 +2,6 @@ import pandas as pd
 
 tabela = pd.read_csv('ClientesBanco.csv', encoding = 'Latin1')
 tabela = tabela.drop('CLIENTNUM', axis=1)
-print(tabela)
+tabela = tabela.dropna()
+
+print(tabela.info())
