3b061ee814dfa38597891530d66359d738ebe9df
Henry Soares <Henrysoares0723@gmail.com>
2026-09-24 21:50:09 -0300

    Avaliando possiveis motivos de cancelamento de clientes e criando grafico para avaliação

--- Codigo.py
+++ Codigo.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import plotly.express as px 
 
 tabela = pd.read_csv('ClientesBanco.csv', encoding = 'Latin1')
 tabela = tabela.drop('CLIENTNUM', axis=1)
@@ -8,3 +9,6 @@ print(tabela.describe())
 
 qtd_categoria_perc = tabela['Categoria'].value_counts(normalize=True).round(2)
 print(qtd_categoria_perc)
+
+grafico = px.histogram(tabela, x='Idade', color='Categoria' )
+grafico.show()
