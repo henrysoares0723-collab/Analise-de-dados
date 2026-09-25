059c8849c7e4db1218c56ef2a58dd13553c6386c
Henry Soares <Henrysoares0723@gmail.com>
2026-09-24 21:38:32 -0300

    Avaliando divisão entre cliente x cancelados

--- Codigo.py
+++ Codigo.py
@@ -3,5 +3,8 @@ import pandas as pd
 tabela = pd.read_csv('ClientesBanco.csv', encoding = 'Latin1')
 tabela = tabela.drop('CLIENTNUM', axis=1)
 tabela = tabela.dropna()
-
 print(tabela.info())
+print(tabela.describe())
+
+qtd_categoria_perc = tabela['Categoria'].value_counts(normalize=True).round(2)
+print(qtd_categoria_perc)
