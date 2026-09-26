506c18c3e8b1af080893a242182596e13975993a
Henry Soares <Henrysoares0723@gmail.com>
2026-09-26 11:13:13 -0300

    Gerando conclusões da analise de dados

--- Codigo.py
+++ Codigo.py
@@ -13,3 +13,6 @@ print(qtd_categoria_perc)
 for coluna in tabela:
     grafico = px.histogram(tabela, x=coluna, color='Categoria' )
     grafico.show()
+
+#De acordo com analise dos graficos noata-se que quanto mia sprodutos contratados um cliente tem, menor a chance dele cancelar, e quanto mais transações e maior o vsalor de transações, menor a chance de cancelar
+#Quanto maior a quantidade de contatos que a pessoa teve que fazer, maior a chance de ser cancelado 
